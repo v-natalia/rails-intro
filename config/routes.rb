df8591f39966e3246000a1020fda1e97ec0003f7
nataliavega <nvegar@gmail.com>
2023-06-12 10:42:51 +0200

    router, controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # page about, verb, url, controller?
+  # home page, contact page
+  get '/about', to: 'pages#about'
+  root to: 'pages#home' # get '/'
+  get '/contact', to: 'pages#contact'
+
 end
